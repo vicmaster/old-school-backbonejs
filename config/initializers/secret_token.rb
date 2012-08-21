933de1d1dd81bd02f9cd43325d990cd28651bd66
Victor Velazquez <victor.velazquez@crowdint.com>
2012-08-21 06:42:12 -0500

    Add backbone old school

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoBackboneApp::Application.config.secret_token = 'e63052aa684259930684429642cd69541567d263a8ebe6cec42ca9f23afaef7cbf9d66c310780b28c61393bb5d3ee9e75e49e00916759e4e7b05e9629691d6a8'
